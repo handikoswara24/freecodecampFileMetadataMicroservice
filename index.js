9744397019fa46139503a338a7531dbfefb63594
handi.koswara <handi.koswara@technosoftautomotive.com>
2022-06-07 21:40:56 +0700

    solution

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var cors = require('cors');
 require('dotenv').config()
 
 var app = express();
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +14,13 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post("/api/fileanalyse",upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.filename,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 
 const port = process.env.PORT || 3000;
